950d1690447d51841e37bd213411228e3dc2a50c
Julian Fang <julianisabear@gmail.com>
2019-05-24 14:48:47 +0800

    start tracking

---
+++ input/zero_error.c
@@ -0,0 +1,42 @@
+/*
+ * 2019 Spring Compiler Course Assignment 2 
+ */
+
+float c = 1.5;
+
+bool loop(int n, int m) {
+    while (n > m) {
+        n--;
+    }
+    return true;
+}
+
+int main() {
+    // Declaration
+    int x;
+    int i;
+    int c;
+    int a = 5;
+    string y = "She is a girl";
+    int k = x + a/(x+i);
+    print(y); // print
+
+    // if condition
+    if (a > 10) {
+        {int asfdasfd;
+            {int sadfasdfasf;
+            }}
+        x += a;
+        print(x);
+    } else if(1){
+        x = a % 10 + 10 * 7; /* Arithmetic */
+        print(x);
+    }else if(1){
+    }else
+    { a = 0;
+        int k = 0; }
+    loop(x, i);
+    print("Hello World");
+
+    return 0; 
+}
